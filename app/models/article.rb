9926f5d17c4d522e2beb7452b447f40e62e2d8e9
Diego Massarni <webdiegomassarini@gmail.com>
2024-10-16 10:44:24 +1000

    feat: add comments on articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  include Visible
+  # depende destroy mean that if we delete an article, we also delete all of its comments
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_15_135946) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_16_004208) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_15_135946) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
